70589846f82acaa288dec983b90e15b6ddc9e83d
shobhit <shobhitsirohi.dev@gmail.com>
2021-09-07 04:41:38 +0530

    404 page first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        yellow: "rgb(255, 230, 148)",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
